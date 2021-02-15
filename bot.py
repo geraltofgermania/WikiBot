a95f5e919709460bd3aee7fe8838950c7712bf4b
geraltofgermania <63039140+geraltofgermania@users.noreply.github.com>
2021-02-15 14:59:14 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,49 @@
-# discord bot
+# discord bot stuff
 import os
 import random
 import discord
 from dotenv import load_dotenv
+
 # web scraper
 import requests
 from bs4 import BeautifulSoup as BS
+
 # discord client
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 bot = '!hermes'
+
 # web scraper
 source = "https://www.wikipedia.org/wiki/"
+
 # menu
 def printMenu():
     return "Hello, here are some things I can do:\n1. Look up something on Wikipedia" \
            "\n\t[EX: !hermes 1 What_To_Look_Up]"
+
 # scrape wiki
 def scrapeWiki(arguments):
     # handle the 2000 limit on discord message
+    # connect to wikipedia
     request = requests.get(source+arguments[2])
     soup = BS(request.text, "html.parser")
+    # store <p>
     response = []
+
+    # get all <p> and store their content in response
     for p in soup.find_all("p"):
         text = p.get_text()
         text = text.replace("\n","")
         response.append(text)
+
+    # get rid of the emtpy lines
     for i in response:
         if i == "" or i == "\n":
             response.remove(i)
+
+    #return array to send in messages
     return response
 
 
